31c919cd1ad06a86a86d107abf8fe54a509a17e5
csmqbusy <cosmiqeyes@yandex.by>
2025-01-22 17:04:45 +0300

    feat: add router to app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.api import router as api_router
 from app.core.config import settings
 
 
 app = FastAPI()
+app.include_router(
+        api_router,
+        prefix=settings.api.prefix
+)
 
 
 if __name__ == '__main__':
