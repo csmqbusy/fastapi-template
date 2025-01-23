4b24ceb75a7164f977ebcc8208ad168815a77ca9
csmqbusy <cosmiqeyes@yandex.by>
2025-01-23 15:49:40 +0300

    feat: add a function to load the .env file correctly

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,18 @@
+from pathlib import Path
+
 from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+def get_correct_path(sources_dir_name: str = "app") -> Path:
+    """
+    This function is needed to correctly launch the application from
+    the app/main.py file and alembic commands from the project root folder.
+    """
+    cwd = Path.cwd()
+    if cwd.name == sources_dir_name:
+        cwd = cwd.parent
+    return cwd
 
 
 class RunConfig(BaseModel):
@@ -21,7 +34,7 @@ class DatabaseConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-            env_file="../.env.dev",
+            env_file=get_correct_path("app") / ".env.dev",
             case_sensitive=False,
             env_nested_delimiter="__",
     )
